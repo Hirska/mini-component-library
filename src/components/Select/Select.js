f8a1269cef121a217c62dd8840053ed19902eb07
Hirska <aleksij.hirvonen@pp.inet.fi>
2021-10-10 19:42:06 +0300

    Select with styles and PresentationalSelect

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -11,35 +11,57 @@ const Select = ({ label, value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <VisuallyHidden>{label}</VisuallyHidden>
-      <SelectIcon id="chevron-down"></SelectIcon>
-      <SelectWrapper value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </SelectWrapper>
+      </NativeSelect>
+      <PresentationalSelect tabIndex="-1">
+        <VisuallyHidden>Select {label}</VisuallyHidden>
+        {displayedValue}
+        <SelectIcon id="chevron-down" size={24} strokeWidth={2}></SelectIcon>
+      </PresentationalSelect>
     </Wrapper>
   );
 };
 
-const SelectWrapper = styled.select`
+const NativeSelect = styled.select`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalSelect = styled.div`
   color: ${COLORS.gray700};
   border-radius: 8px;
   height: 43px;
   border: none;
   background-color: ${COLORS.transparentGray15};
-  padding: 12px 16px;
+  padding: 12px 52px 12px 16px;
   appearance: none;
-  &:hover {
+  ${NativeSelect}:focus + & {
+    outline: ${COLORS.primary} solid 2px;
+  }
+
+  ${NativeSelect}:hover + & {
     color: revert;
   }
 `;
 
 const SelectIcon = styled(Icon)`
   position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
   margin: auto;
+  margin-right: 4px;
 `;
 
 const Wrapper = styled.div`
   position: relative;
+  width: max-content;
 `;
 
 export default Select;
